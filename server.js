e83d9f7b094af7470f0420dfb4a2894cfb878657
HighPriest19 <kryptonolagunju@gmail.com>
2026-05-10 19:40:21 +0100

    commit

--- server.js
+++ server.js
@@ -11,30 +11,39 @@ app.set("view engine", "ejs")
 // Serve static files (CSS/Images) from the public folder
 app.use(express.static("public"))
 
+// Serve images from the images folder
+app.use("/images", express.static("images"))
+
 /* ***********************
  * Routes
  *************************/
 
 // Home Page
 app.get("/", async (req, res) => {
-  res.render("index", { title: "Home" })
+  res.render("index", { title: "Home", year: new Date().getFullYear() })
 })
 
 // Organizations Page
 app.get("/organizations", async (req, res) => {
-  res.render("organizations", { title: "Organizations" })
+  const organizations = [
+    { name: "Habitat for Humanity", description: "Building homes and communities", image: "/images/organization1.jpeg" },
+    { name: "Red Cross", description: "Humanitarian assistance and disaster relief", image: "/images/organization2.jpeg" },
+    { name: "Local Food Bank", description: "Fighting hunger in our community", image: "/images/organization3.jpeg" }
+  ]
+  res.render("organizations", { title: "Organizations", organizations: organizations, year: new Date().getFullYear() })
 })
 
 // Service Projects Page
 app.get("/projects", async (req, res) => {
-  res.render("projects", { title: "Projects" })
+  res.render("projects", { title: "Projects", year: new Date().getFullYear() })
 })
 
 // Categories Page (REQUIRED for Assignment)
 app.get("/categories", async (req, res) => {
-  res.render("categories", { title: "Categories" })
+  res.render("categories", { title: "Categories", year: new Date().getFullYear() })
 })
 
 app.listen(port, () => {
   console.log(`App listening on http://localhost:${port}`)
 })
+
